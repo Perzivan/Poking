e34758363f066d9d613d89934a1f701db6b8d32c
joakimerlandsson <unknown-user@10.0.1.5>
2013-11-30 17:59:29 +0100

    Correcting the animation.

--- Poking/PokingViewController.cs
+++ Poking/PokingViewController.cs
@@ -4,6 +4,9 @@ using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using MonoTouch.CoreAnimation;
+using MonoTouch.CoreGraphics;
+
 
 
 namespace Poking
@@ -16,7 +19,7 @@ namespace Poking
 
 		public PokingViewController () : base ("PokingViewController", null)
 		{
-					
+
 		}
 
 		public override void DidReceiveMemoryWarning ()
@@ -29,16 +32,20 @@ namespace Poking
 
 		public override void TouchesEnded (NSSet touches, UIEvent evt)
 		{
+			if (!_Timer.Enabled)
+				return;
+
 			base.TouchesEnded (touches, evt);
 			// get the touch
 			UITouch touch = touches.AnyObject as UITouch;
 
 			if (touch != null) {
-				//==== IMAGE TOUCH
-				if (_Smiley.PokeImage.ImageView.Frame.Contains(touch.LocationInView(View)))
-				{
+				PointF Location = touch.LocationInView(View);
+		
+				if (_Smiley.Layer.Frame.Contains (Location)) {
 					AddPoints (1);
-					UIView.Animate (0.5,0,UIViewAnimationOptions.CurveEaseIn,MoveSmiley,()=>{});
+					UIView.Animate (0.1, 0, UIViewAnimationOptions.CurveEaseIn, MoveSmiley, () => {
+					});
 				}
 			}
 		}
@@ -49,18 +56,20 @@ namespace Poking
 
 			// Perform any additional setup after loading the view, typically from a nib.
 			_Board = new Board (262, 365);
-
 			_Smiley = new Smiley ("smiley.png",34, 34,_Board.Width ,_Board.Height );
-			Add (_Smiley.PokeImage.ImageView);
+
+			View.Layer.AddSublayer (_Smiley.Layer);
 
 			_Timer = new System.Timers.Timer (2000);
 			//hmm is this aproach wrong? how are these kinds of timed animations best done. 
+			//How can I move the Image when it's on another thread?
 			_Timer.Elapsed += (sender, e) => {
+				CATransaction.Begin ();
 				MoveSmiley();
+				CATransaction.Commit ();
 			};
 
 			lblPoints.Text = "Points: 0";
-
 		}
 	
 		public void AddPoints(int points){
@@ -72,16 +81,22 @@ namespace Poking
 
 		partial void btnStart (NSObject sender)
 		{
-			_Timer.Start();		
+			_Timer.Start();
+			MoveSmiley();		
 		}
 		 
 		private void MoveSmiley() 
 		{
-			int width = _Board.Width - Convert.ToInt32(_Smiley.PokeImage.ImageView.Frame.Width);
-			int height = _Board.Height - Convert.ToInt32(_Smiley.PokeImage.ImageView.Frame.Height);
+			int width = _Board.Width - Convert.ToInt32(_Smiley.Layer.Frame.Width);
+			int height = _Board.Height - Convert.ToInt32(_Smiley.Layer.Frame.Height);
 
-			Point p = _Smiley.GetNextPossition (width, height);
-			_Smiley.PokeImage.ImageView.Frame = new RectangleF (p.X, p.Y, _Smiley.Rect.Width, _Smiley.Rect.Height);
+			CATransaction.Begin ();
+			CATransaction.AnimationDuration = 2;
+
+			_Smiley.Layer.Position = _Smiley.GetNextPossition (width, height);
+
+			CATransaction.Commit ();
+
 		}
 
 		partial void btnStop (NSObject sender)
--- Poking/Smiley.cs
+++ Poking/Smiley.cs
@@ -1,32 +1,35 @@
 using System;
 using System.Drawing;
 using MonoTouch.UIKit;
-
+using MonoTouch.CoreAnimation;
 
 namespace Poking
 {
 	public class Smiley {
 		public RectangleF Rect { get;private set;}
-		public UIImage Image { get;private set;}
-		public PokeImage PokeImage { get;private set;}
+		public CALayer Layer { get;private set;}
 		private int MaxMoveX;
 		private int MaxMoveY;
 
 		public Smiley(string imagePath, int width, int height, int maxMoveX, int maxMoveY) {
 			MaxMoveX = maxMoveX;
 			MaxMoveY = maxMoveY;
-			Point p = GetNextPossition (maxMoveX - width, maxMoveY - height);
+			PointF p = GetNextPossition (maxMoveX - width, maxMoveY - height);
 			Rect = new RectangleF (p.X, p.Y, width, height);
-			Image = new UIImage (imagePath);
-			PokeImage = new PokeImage (Rect, Image);
+
+			Layer = new CALayer ();
+			Layer.Bounds = new RectangleF (0, 0, 50, 50);
+			Layer.Position = GetNextPossition (maxMoveX, maxMoveY);
+			Layer.Contents = UIImage.FromFile ("smiley.png").CGImage;
+			Layer.ContentsGravity = CALayer.GravityResizeAspectFill;
 		}
 
 		public void RandomMove(int width, int height) {
-			Point p = GetNextPossition (MaxMoveX - width, MaxMoveY - height);
+			PointF p = GetNextPossition (MaxMoveX - width, MaxMoveY - height);
 			Rect = new RectangleF (p.X, p.Y, width, height);
 		}
 
-		public Point GetNextPossition(int maxX, int maxY) {
+		public PointF GetNextPossition(int maxX, int maxY) {
 			System.Random rnd = new Random ();
 			int x = rnd.Next (0, maxX);
 			int y = rnd.Next (0, maxY);
